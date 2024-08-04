ed247455685b6e2c347264c1a7c86f58234c3be6
Ahmed Wael <awael3977@gmail.com>
2024-08-04 17:09:17 +0300

    revert: remove clerk and add next-auth to the app

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,47 +1,29 @@
-import ClerkLoadingContent from "@/components/ClerkLoadingAndLoaded/ClerkLoadingContent";
 import NavLinks from "@/components/NavLinks";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { appName } from "@/utils/flags";
 import { urls } from "@/utils/urls";
-import {
-	SignedIn,
-	SignedOut,
-	SignIn,
-	SignInButton,
-	UserButton,
-} from "@clerk/nextjs";
 import { Cookie } from "next/font/google";
 import Link from "next/link";
 
 const cookie = Cookie({
-	weight: "400",
-	subsets: ["latin"],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export default function Header() {
-	return (
-		<header className="border-b-2">
-			<nav className="p-6 container flex items-center gap-6">
-				<Link
-					href={urls.home.href}
-					title={appName}
-					className={cn(cookie.className, "text-3xl mr-auto")}
-				>
-					{appName}
-				</Link>
-				<NavLinks />
-				<ClerkLoadingContent>
-					<SignedOut>
-						<SignInButton>
-							<Button className="font-bold">Sign in</Button>
-						</SignInButton>
-					</SignedOut>
-					<SignedIn>
-						<UserButton />
-					</SignedIn>
-				</ClerkLoadingContent>
-			</nav>
-		</header>
-	);
+  return (
+    <header className="border-b-2">
+      <nav className="container flex items-center gap-6 p-6">
+        <Link
+          href={urls.home.href}
+          title={appName}
+          className={cn(cookie.className, "mr-auto text-3xl")}
+        >
+          {appName}
+        </Link>
+        <NavLinks />
+      </nav>
+    </header>
+  );
 }
