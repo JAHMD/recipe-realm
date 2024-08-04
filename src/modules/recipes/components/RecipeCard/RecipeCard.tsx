8f2f5930f109649278e4e24358cb9dad2e800063
Ahmed Wael <awael3977@gmail.com>
2024-08-04 14:37:10 +0300

    style: Add new ui components and update recipe card ui

--- src/modules/recipes/components/RecipeCard/RecipeCard.tsx
+++ src/modules/recipes/components/RecipeCard/RecipeCard.tsx
@@ -1,16 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import { urls } from "@/utils/urls";
+import Image from "next/image";
 import Link from "next/link";
-import { Recipe } from "../../utils/types";
+import { RecipeAlt } from "../../utils/types";
 
 export type RecipeCardProps = {
-	recipe: Recipe;
+	recipe: RecipeAlt;
 };
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
 	return (
-		<Link href={urls.recipes.view(recipe.id)} className="border rounded-md p-4">
-			<h2>{recipe.title}</h2>
-			<p className="">{recipe.description}</p>
-		</Link>
+		<Card>
+			<CardContent className="p-4">
+				{/* <Image
+					src={recipe.image.url}
+					alt={`${recipe.title} image`}
+					width={600}
+					height={600}
+				/> */}
+				<Link
+					href={urls.recipes.view(recipe.id)}
+					className="flex flex-col gap-2"
+				>
+					<CardTitle>{recipe.title}</CardTitle>
+					<CardDescription className="text-muted-foreground line-clamp-2">
+						{recipe.description}
+					</CardDescription>
+				</Link>
+				<div className="flex justify-between gap-2 text-sm mt-6 text-muted-foreground">
+					<span>Cook: {recipe.cookingTime}</span>
+					{recipe.difficulty && <span>{recipe.difficulty}</span>}
+				</div>
+			</CardContent>
+			<CardFooter>
+				<div className="">
+					<span>{recipe.category}</span>
+				</div>
+				{/* <div className="">
+					<Button>Bookmark</Button>
+					<Button>Share</Button>
+				</div> */}
+			</CardFooter>
+		</Card>
 	);
 }
